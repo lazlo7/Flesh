31457aaa3266e8090a721429cd15fba7aafff5bb
Max Smagin <lazlogogalac777@gmail.com>
2023-07-02 22:49:20 +0300

    Removed redundant OvergrownModel

--- src/client/kotlin/net/requef/flesh/model/OvergrownModel.kt
+++
@@ -1,7 +0,0 @@
-package net.requef.flesh.model
-
-import net.minecraft.client.model.ModelPart
-import net.minecraft.client.render.entity.model.ZombieEntityModel
-import net.requef.flesh.mob.Overgrown
-
-class OvergrownModel(modelPart: ModelPart) : ZombieEntityModel<Overgrown>(modelPart)
