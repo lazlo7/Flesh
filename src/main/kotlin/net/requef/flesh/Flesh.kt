e569a24b8ec0000ab6019688799879eb31a3b920
Max Smagin <lazlogogalac777@gmail.com>
2023-07-02 17:53:01 +0300

    Removed default hello world from onInitialize()

--- src/main/kotlin/net/requef/flesh/Flesh.kt
+++ src/main/kotlin/net/requef/flesh/Flesh.kt
@@ -7,9 +7,6 @@ object Flesh : ModInitializer {
     private val logger = LoggerFactory.getLogger("flesh")
 
 	override fun onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		logger.info("Hello Fabric world!")
+		
 	}
 }
