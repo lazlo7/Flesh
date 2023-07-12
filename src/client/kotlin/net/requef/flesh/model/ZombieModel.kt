374c8d919017a9277154ebbc03b106bc8487bbff
Max Smagin <lazlogogalac777@gmail.com>
2023-07-12 03:45:19 +0300

    Added armor rendering for regular zombie

--- src/client/kotlin/net/requef/flesh/model/ZombieModel.kt
+++ src/client/kotlin/net/requef/flesh/model/ZombieModel.kt
@@ -5,7 +5,6 @@ import software.bernie.geckolib.constant.DataTickets
 import software.bernie.geckolib.core.animatable.GeoAnimatable
 import software.bernie.geckolib.core.animation.AnimationState
 import software.bernie.geckolib.model.GeoModel
-import software.bernie.geckolib.model.data.EntityModelData
 
 open class ZombieModel<T> : GeoModel<T>() where T: GeoAnimatable {
     override fun getModelResource(animatable: T) = Flesh.identifier("geo/zombie.geo.json")
@@ -17,19 +16,13 @@ open class ZombieModel<T> : GeoModel<T>() where T: GeoAnimatable {
     override fun setCustomAnimations(
         animatable: T,
         instanceId: Long,
-        animationState: AnimationState<T>?
+        animationState: AnimationState<T>
     ) {
-        super.setCustomAnimations(animatable, instanceId, animationState)
-
-        if (animationState == null) {
-            return
-        }
-
         val head = animationProcessor.getBone("head")
         if (head != null) {
-            val extraData = animationState.extraData[DataTickets.ENTITY_MODEL_DATA] as EntityModelData
-            head.rotX = extraData.headPitch * Math.PI.toFloat() / 180.0f
-            head.rotY = extraData.netHeadYaw * Math.PI.toFloat() / 180.0f
+            val data = animationState.getData(DataTickets.ENTITY_MODEL_DATA)
+            head.rotX = data.headPitch * Math.PI.toFloat() / 180.0f
+            head.rotY = data.netHeadYaw * Math.PI.toFloat() / 180.0f
         }
     }
 }
--- src/client/kotlin/net/requef/flesh/render/ZombieRenderer.kt
+++ src/client/kotlin/net/requef/flesh/render/ZombieRenderer.kt
@@ -2,8 +2,10 @@ package net.requef.flesh.render
 
 import net.minecraft.client.render.VertexConsumerProvider
 import net.minecraft.client.render.entity.EntityRendererFactory
+import net.minecraft.client.render.entity.model.BipedEntityModel
 import net.minecraft.client.render.model.json.ModelTransformationMode
 import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.entity.EquipmentSlot
 import net.minecraft.entity.mob.MobEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.util.math.RotationAxis
@@ -13,13 +15,66 @@ import software.bernie.geckolib.core.animatable.GeoAnimatable
 import software.bernie.geckolib.model.GeoModel
 import software.bernie.geckolib.renderer.GeoEntityRenderer
 import software.bernie.geckolib.renderer.layer.BlockAndItemGeoLayer
+import software.bernie.geckolib.renderer.layer.ItemArmorGeoLayer
 
 open class ZombieRenderer<T>(ctx: EntityRendererFactory.Context, model: GeoModel<T> = ZombieModel())
     : GeoEntityRenderer<T>(ctx, model) where T: MobEntity, T: GeoAnimatable {
+    val armorRightBootName = "right_leg_boots"
+    val armorLeftBootName = "left_leg_boots"
+
+    val armorRightLegName = "right_leg_armor"
+    val armorLeftLegName = "left_leg_armor"
+
+    val armorRightArmName = "right_arm"
+    val armorLeftArmName = "left_arm"
+    val armorBodyName = "body_armor"
+
+    val armorHeadName = "head_armor"
+
     val rightHeldItemName = "right_arm_held_item"
     val leftHeldItemName = "left_arm_held_item"
 
     init {
+        renderLayers.addLayer(object : ItemArmorGeoLayer<T>(this) {
+            override fun getArmorItemForBone(bone: GeoBone, animatable: T) = when(bone.name) {
+                armorRightBootName, armorLeftBootName -> bootsStack
+                armorRightLegName, armorLeftLegName -> leggingsStack
+                armorBodyName, armorRightArmName, armorLeftArmName -> chestplateStack
+                armorHeadName -> helmetStack
+                else -> null
+            }
+
+            override fun getEquipmentSlotForBone(
+                bone: GeoBone,
+                stack: ItemStack?,
+                animatable: T
+            ): EquipmentSlot = when(bone.name) {
+                armorRightBootName, armorLeftBootName -> EquipmentSlot.FEET
+                armorRightLegName, armorLeftLegName -> EquipmentSlot.LEGS
+                armorRightArmName -> if (animatable.isLeftHanded) EquipmentSlot.OFFHAND else EquipmentSlot.MAINHAND
+                armorLeftArmName -> if (animatable.isLeftHanded) EquipmentSlot.MAINHAND else EquipmentSlot.OFFHAND
+                armorBodyName -> EquipmentSlot.CHEST
+                armorHeadName -> EquipmentSlot.HEAD
+                else -> super.getEquipmentSlotForBone(bone, stack, animatable)
+            }
+
+            override fun getModelPartForBone(
+                bone: GeoBone,
+                slot: EquipmentSlot?,
+                stack: ItemStack?,
+                animatable: T,
+                baseModel: BipedEntityModel<*>
+            ) = when (bone.name) {
+                armorRightBootName, armorRightLegName -> baseModel.rightLeg
+                armorLeftBootName, armorLeftLegName -> baseModel.leftLeg
+                armorRightArmName -> baseModel.rightArm
+                armorLeftArmName -> baseModel.leftArm
+                armorBodyName -> baseModel.body
+                armorHeadName -> baseModel.head
+                else -> super.getModelPartForBone(bone, slot, stack, animatable, baseModel)
+            }
+        })
+
         renderLayers.addLayer(object : BlockAndItemGeoLayer<T>(this) {
             override fun getStackForBone(bone: GeoBone, animatable: T): ItemStack? = when(bone.name) {
                 leftHeldItemName -> if (animatable.isLeftHanded) animatable.mainHandStack else animatable.offHandStack
