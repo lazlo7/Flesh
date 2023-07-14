0cdfbef62b6a249468859ef79a6c9f2fdc681216
Max Smagin <lazlogogalac777@gmail.com>
2023-07-14 21:39:55 +0300

    Improved no-baby spawning mechanism for zombies

--- src/main/kotlin/net/requef/flesh/entity/Zombie.kt
+++ src/main/kotlin/net/requef/flesh/entity/Zombie.kt
@@ -1,8 +1,13 @@
 package net.requef.flesh.entity
 
+import net.minecraft.entity.EntityData
 import net.minecraft.entity.EntityType
+import net.minecraft.entity.SpawnReason
 import net.minecraft.entity.attribute.DefaultAttributeContainer
 import net.minecraft.entity.mob.ZombieEntity
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.world.LocalDifficulty
+import net.minecraft.world.ServerWorldAccess
 import net.minecraft.world.World
 import software.bernie.geckolib.animatable.GeoEntity
 import software.bernie.geckolib.core.animatable.GeoAnimatable
@@ -53,9 +58,17 @@ open class Zombie(entityType: EntityType<out ZombieEntity>, world: World) : Zomb
 
     override fun getAnimatableInstanceCache() = cache
 
-    override fun isBaby() = false
-
-    override fun setBaby(baby: Boolean) {
-
+    override fun initialize(
+        world: ServerWorldAccess?,
+        difficulty: LocalDifficulty?,
+        spawnReason: SpawnReason?,
+        entityData: EntityData?,
+        entityNbt: NbtCompound?
+    ): EntityData? {
+        // Minecraft zombie class tries to make the zombie a baby in the super-method.
+        // It only does it if the provided entity data is an instance of ZombieData.
+        // Flesh zombies can't be babies - by providing a plain entity data object,
+        // the super-method skips the attempt to make the entity a baby.
+        return super.initialize(world, difficulty, spawnReason, object : EntityData {}, entityNbt)
     }
 }
