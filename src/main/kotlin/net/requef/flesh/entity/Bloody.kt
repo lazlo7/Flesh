f6cd25c3da75e358c7264a9e8193cbeaa23a53f3
Max Smagin <lazlogogalac777@gmail.com>
2023-07-15 19:42:08 +0300

    Added lunging melee attack for bloody zombie

--- src/main/kotlin/net/requef/flesh/entity/Bloody.kt
+++ src/main/kotlin/net/requef/flesh/entity/Bloody.kt
@@ -1,11 +1,13 @@
 package net.requef.flesh.entity
 
 import net.minecraft.entity.EntityType
+import net.minecraft.entity.ai.goal.ZombieAttackGoal
 import net.minecraft.entity.attribute.EntityAttribute
 import net.minecraft.entity.attribute.EntityAttributeModifier
 import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.entity.mob.ZombieEntity
 import net.minecraft.world.World
+import net.requef.flesh.ai.BloodyZombieAttackGoal
 
 class Bloody(val type: Type, entityType: EntityType<out ZombieEntity>, world: World) : Zombie(entityType, world) {
     companion object {
@@ -17,6 +19,13 @@ class Bloody(val type: Type, entityType: EntityType<out ZombieEntity>, world: Wo
         }
 
         fun createBloodyAttributes() = createFleshZombieAttributes()
+    }
+
+    override fun initGoals() {
+        super.initGoals()
+        // Replace default zombie attack goal with bloody zombie's specific one.
+        goalSelector.clear { goal -> goal is ZombieAttackGoal }
+        goalSelector.add(2, BloodyZombieAttackGoal(this, 1.0, 1.0))
     }
 
     override fun applyAttributeModifiers(chanceMultiplier: Float) {
@@ -36,6 +45,14 @@ class Bloody(val type: Type, entityType: EntityType<out ZombieEntity>, world: Wo
         health = getAttributeValue(EntityAttributes.GENERIC_MAX_HEALTH).toFloat()
         apply(EntityAttributes.GENERIC_MOVEMENT_SPEED, type.speedIncrease)
         apply(EntityAttributes.GENERIC_ATTACK_DAMAGE, type.attackDamageIncrease)
+    }
+
+    override fun onLanding() {
+        super.onLanding()
+        if (target != null) {
+            navigation.recalculatePath()
+            lookControl.lookAt(target)
+        }
     }
 
     enum class Type(
