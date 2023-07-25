aaa62504a70f9c1a3681288bcb9fddb4a2ff2cbf
Max Smagin <lazlogogalac777@gmail.com>
2023-07-25 18:34:42 +0300

    Made archer use SBL

--- src/main/kotlin/net/requef/flesh/entity/Archer.kt
+++ src/main/kotlin/net/requef/flesh/entity/Archer.kt
@@ -4,14 +4,10 @@ import net.minecraft.entity.EntityType
 import net.minecraft.entity.EquipmentSlot
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.ai.RangedAttackMob
-import net.minecraft.entity.ai.goal.Goal
-import net.minecraft.entity.ai.goal.ZombieAttackGoal
+import net.minecraft.entity.ai.brain.MemoryModuleType
 import net.minecraft.entity.attribute.DefaultAttributeContainer
 import net.minecraft.entity.attribute.EntityAttributes
-import net.minecraft.entity.mob.HostileEntity
-import net.minecraft.entity.mob.MobEntity
 import net.minecraft.entity.mob.ZombieEntity
-import net.minecraft.entity.projectile.ProjectileUtil
 import net.minecraft.item.BowItem
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
@@ -20,10 +16,18 @@ import net.minecraft.sound.SoundEvents
 import net.minecraft.util.math.random.Random
 import net.minecraft.world.LocalDifficulty
 import net.minecraft.world.World
+import net.tslat.smartbrainlib.api.core.BrainActivityGroup
+import net.tslat.smartbrainlib.api.core.behaviour.custom.attack.BowAttack
+import net.tslat.smartbrainlib.api.core.behaviour.custom.look.LookAtTarget
+import net.tslat.smartbrainlib.api.core.behaviour.custom.misc.AvoidSun
+import net.tslat.smartbrainlib.api.core.behaviour.custom.move.EscapeSun
+import net.tslat.smartbrainlib.api.core.behaviour.custom.move.MoveToWalkTarget
+import net.tslat.smartbrainlib.api.core.behaviour.custom.move.StrafeTarget
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.InvalidateAttackTarget
+import net.tslat.smartbrainlib.util.BrainUtils
 import software.bernie.geckolib.core.animatable.GeoAnimatable
 import software.bernie.geckolib.core.animation.*
 import software.bernie.geckolib.core.`object`.PlayState
-import java.util.*
 import kotlin.math.sqrt
 
 class Archer(entityType: EntityType<out ZombieEntity>, world: World)
@@ -55,13 +59,6 @@ class Archer(entityType: EntityType<out ZombieEntity>, world: World)
         }
 
         return PlayState.CONTINUE
-    }
-
-    override fun initGoals() {
-        super.initGoals()
-        // Remove melee zombie attack goal because this is a ranged zombie.
-        goalSelector.clear { goal -> goal is ZombieAttackGoal }
-        goalSelector.add(2, ArcherBowAttackGoal(this, 1.15, 25, 40, bowRange))
     }
 
     override fun initEquipment(random: Random, localDifficulty: LocalDifficulty) {
@@ -98,107 +95,46 @@ class Archer(entityType: EntityType<out ZombieEntity>, world: World)
 
     override fun canUseRangedWeapon(weapon: RangedWeaponItem) = weapon is BowItem
 
-    class ArcherBowAttackGoal<T>(
-        private val actor: T,
-        private val speed: Double,
-        private val arrowCooldownTicks: Int,
+    override fun getCoreTasks(): BrainActivityGroup<out Zombie> = BrainActivityGroup.coreTasks(
+        AvoidSun<Archer>(),
+        EscapeSun<Archer>(),
+        LookAtTarget<Archer>(),
+        StrafeTarget<Archer>()
+            .speedMod(1.15f)
+            .stopStrafingWhen {entity -> !isHoldingBow(entity) }
+            .startCondition(::isHoldingBow),
+        MoveToWalkTarget<Archer>()
+    )
+
+    override fun getFightTasks(): BrainActivityGroup<out Zombie> = BrainActivityGroup.fightTasks(
+        InvalidateAttackTarget<Archer>(),
+        // Zombie archer can't melee attack by design.
+        ArcherBowAttack<Archer>(25, 40)
+            .attackInterval { _ -> 65 }
+            .attackRadius(bowRange)
+    )
+
+    private fun isHoldingBow(entity: LivingEntity) = entity.isHolding {stack -> stack.item is BowItem }
+
+    class ArcherBowAttack<T>(
+        arrowCooldownTicks: Int,
         private val aimingDurationTicks: Int,
-        range: Float
-    ) : Goal() where T: HostileEntity, T: RangedAttackMob {
-        private val squaredRange: Float
-        private var arrowCooldown = 0
-        private var targetSeeingTicker = 0
-        private var combatTicks = -1
-        private var movingToLeft = false
-        private var backward = false
+    ) : BowAttack<T>(arrowCooldownTicks) where T: LivingEntity, T: RangedAttackMob {
+        override fun doDelayedAction(entity: T) {
+            if (target == null
+                || !BrainUtils.canSee(entity, target)
+                || entity.squaredDistanceTo(target) > attackRadius
+                || entity.itemUseTime < aimingDurationTicks) return
 
-        init {
-            squaredRange = range * range
-            controls = EnumSet.of(Control.MOVE, Control.LOOK)
-        }
+            entity.attack(target, BowItem.getPullProgress(entity.itemUseTime))
+            entity.clearActiveItem()
 
-        override fun canStart() = actor.target != null && isHoldingBow()
-
-        private fun isHoldingBow() = actor.isHolding(Items.BOW)
-
-        override fun shouldContinue() = isHoldingBow() && (canStart() || !actor.navigation.isIdle)
-
-        override fun start() {
-            super.start()
-            actor.isAttacking = true
-        }
-
-        override fun stop() {
-            super.stop()
-            actor.isAttacking = false
-            targetSeeingTicker = 0
-            arrowCooldown = -1
-            actor.clearActiveItem()
-        }
-
-        override fun shouldRunEveryTick() = true
-
-        override fun tick() {
-            val target = actor.target ?: return
-            val dist = actor.squaredDistanceTo(target.x, target.y, target.z)
-
-            val seeingTarget = actor.visibilityCache.canSee(target)
-            val seenTargetEnough = targetSeeingTicker > 0
-            if (seeingTarget != seenTargetEnough) {
-                targetSeeingTicker = 0
-            }
-
-            targetSeeingTicker += if (seeingTarget) 1 else -1
-
-            if (dist > squaredRange.toDouble() || targetSeeingTicker < 20) {
-                actor.navigation.startMovingTo(target, speed)
-                combatTicks = -1
-            } else {
-                actor.navigation.stop()
-                combatTicks++
-            }
-
-            if (combatTicks >= 20) {
-                if (actor.random.nextFloat().toDouble() < 0.3) {
-                    movingToLeft xor true
-                }
-
-                if (actor.random.nextFloat().toDouble() < 0.3) {
-                    backward xor true
-                }
-
-                combatTicks = 0
-            }
-
-            if (combatTicks > -1) {
-                backward = dist < 0.25 * squaredRange
-
-                actor.moveControl.strafeTo(
-                    if (backward) -0.5f else 0.5f,
-                    if (movingToLeft) 0.5f else -0.5f
-                )
-
-                val vehicle = actor.controllingVehicle
-                if (vehicle is MobEntity) {
-                    vehicle.lookAtEntity(target, 30.0f, 30.0f)
-                }
-
-                actor.lookAtEntity(target, 30.0f, 30.0f)
-            } else {
-                actor.lookControl.lookAt(target, 30.0f, 30.0f)
-            }
-
-            if (actor.isUsingItem) {
-                if (!seeingTarget && targetSeeingTicker < -60) {
-                    actor.clearActiveItem()
-                } else if (seeingTarget && actor.itemUseTime >= aimingDurationTicks) {
-                    actor.attack(target, BowItem.getPullProgress(actor.itemUseTime))
-                    actor.clearActiveItem()
-                    arrowCooldown = arrowCooldownTicks
-                }
-            } else if (arrowCooldown-- <= 0 && targetSeeingTicker >= -60) {
-                actor.setCurrentHand(ProjectileUtil.getHandPossiblyHolding(actor, Items.BOW))
-            }
+            BrainUtils.setForgettableMemory(
+                entity,
+                MemoryModuleType.ATTACK_COOLING_DOWN,
+                true,
+                attackIntervalSupplier.apply(entity)
+            )
         }
     }
 }
