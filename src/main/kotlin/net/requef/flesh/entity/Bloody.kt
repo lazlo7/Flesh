4804bc48c4bfa827f9aa686c647184de1026693e
Max Smagin <lazlogogalac777@gmail.com>
2023-07-29 21:52:03 +0300

    Made Bloody use SBL

--- src/main/kotlin/net/requef/flesh/entity/Bloody.kt
+++ src/main/kotlin/net/requef/flesh/entity/Bloody.kt
@@ -1,13 +1,27 @@
 package net.requef.flesh.entity
 
+import com.mojang.datafixers.util.Pair
+import it.unimi.dsi.fastutil.objects.ObjectArrayList
 import net.minecraft.entity.EntityType
-import net.minecraft.entity.ai.goal.ZombieAttackGoal
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.ai.brain.MemoryModuleState
+import net.minecraft.entity.ai.brain.MemoryModuleType
 import net.minecraft.entity.attribute.EntityAttribute
 import net.minecraft.entity.attribute.EntityAttributeModifier
 import net.minecraft.entity.attribute.EntityAttributes
+import net.minecraft.entity.mob.MobEntity
 import net.minecraft.entity.mob.ZombieEntity
+import net.minecraft.server.world.ServerWorld
 import net.minecraft.world.World
-import net.requef.flesh.ai.BloodyZombieAttackGoal
+import net.tslat.smartbrainlib.api.core.BrainActivityGroup
+import net.tslat.smartbrainlib.api.core.behaviour.ExtendedBehaviour
+import net.tslat.smartbrainlib.api.core.behaviour.custom.attack.AnimatableMeleeAttack
+import net.tslat.smartbrainlib.api.core.behaviour.custom.path.SetWalkTargetToAttackTarget
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.InvalidateAttackTarget
+import net.tslat.smartbrainlib.util.BrainUtils
+import java.util.function.BiPredicate
+import kotlin.math.pow
+
 
 class Bloody(val type: Type, entityType: EntityType<out ZombieEntity>, world: World) : Zombie(entityType, world) {
     companion object {
@@ -19,13 +33,6 @@ class Bloody(val type: Type, entityType: EntityType<out ZombieEntity>, world: Wo
         }
 
         fun createBloodyAttributes() = createFleshZombieAttributes()
-    }
-
-    override fun initGoals() {
-        super.initGoals()
-        // Replace default zombie attack goal with bloody zombie's specific one.
-        goalSelector.clear { goal -> goal is ZombieAttackGoal }
-        goalSelector.add(2, BloodyZombieAttackGoal(this, 1.0, 1.0))
     }
 
     override fun applyAttributeModifiers(chanceMultiplier: Float) {
@@ -55,6 +62,18 @@ class Bloody(val type: Type, entityType: EntityType<out ZombieEntity>, world: Wo
         }
     }
 
+    override fun getFightTasks(): BrainActivityGroup<out Zombie> = BrainActivityGroup.fightTasks(
+        InvalidateAttackTarget<Bloody>(),
+        SetWalkTargetToAttackTarget<Bloody>(),
+        AnimatableMeleeAttack<Bloody>(0),
+        LungeTowardsAttackTarget<Bloody>(0.3)
+            .lungePredicate { entity, target ->
+                // From nms: MeleeAttackGoal
+                val maxAttackDistanceSqr = (entity.width * 2.0).pow(2) + target.width
+                entity.getSquaredDistanceToAttackPosOf(target) > 1.5 * maxAttackDistanceSqr }
+            .cooldownFor { 8 * 20 }
+    )
+
     enum class Type(
         val textureName: String,
         val attributeBonusName: String,
@@ -83,5 +102,42 @@ class Bloody(val type: Type, entityType: EntityType<out ZombieEntity>, world: Wo
             0.115,
             3.05
         )
+    }
+
+    class LungeTowardsAttackTarget<T : MobEntity>(private val lungeYSpeed: Double) : ExtendedBehaviour<T>() {
+        companion object {
+            private val memoryRequirements: List<Pair<MemoryModuleType<*>, MemoryModuleState>> =
+                ObjectArrayList.of(Pair.of(MemoryModuleType.ATTACK_TARGET, MemoryModuleState.VALUE_PRESENT))
+        }
+
+        private var lungePredicate: BiPredicate<in MobEntity, LivingEntity> = BiPredicate { _, _ -> true }
+
+        fun lungePredicate(predicate: BiPredicate<in MobEntity, LivingEntity>): LungeTowardsAttackTarget<T> {
+            lungePredicate = predicate
+            return this
+        }
+
+        override fun getMemoryRequirements() = LungeTowardsAttackTarget.memoryRequirements
+
+        override fun shouldRun(level: ServerWorld, entity: T) = lungePredicate.test(
+            // Memory requirements have been already satisfied at this point,
+            // so we can use ATTACK_TARGET memory here.
+            entity, BrainUtils.getMemory(entity, MemoryModuleType.ATTACK_TARGET)!!
+        )
+
+        override fun start(entity: T) {
+            val attackTarget = BrainUtils.getMemory(entity, MemoryModuleType.ATTACK_TARGET)
+
+            if (attackTarget == null) {
+                BrainUtils.clearMemory(entity, MemoryModuleType.ATTACK_TARGET)
+                return
+            }
+
+            val targetPos = attackTarget.pos
+            val vel = targetPos.subtract(entity.pos)
+                .normalize()
+                .add(0.0, lungeYSpeed, 0.0)
+            entity.addVelocity(vel)
+        }
     }
 }
