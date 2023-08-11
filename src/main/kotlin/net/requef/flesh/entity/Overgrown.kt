980f97e158dac89190a29a3e55797b540d95278e
Max Smagin <lazlogogalac777@gmail.com>
2023-08-11 15:00:35 +0300

    Removed armor spawning for overgrown

--- src/main/kotlin/net/requef/flesh/entity/Overgrown.kt
+++ src/main/kotlin/net/requef/flesh/entity/Overgrown.kt
@@ -1,9 +1,17 @@
 package net.requef.flesh.entity
 
+import net.minecraft.entity.EntityData
 import net.minecraft.entity.EntityType
+import net.minecraft.entity.EquipmentSlot
+import net.minecraft.entity.SpawnReason
 import net.minecraft.entity.attribute.DefaultAttributeContainer
 import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.entity.mob.ZombieEntity
+import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.util.math.random.Random
+import net.minecraft.world.LocalDifficulty
+import net.minecraft.world.ServerWorldAccess
 import net.minecraft.world.World
 import software.bernie.geckolib.core.animatable.GeoAnimatable
 import software.bernie.geckolib.core.animation.*
@@ -17,6 +25,26 @@ class Overgrown(entityType: EntityType<out ZombieEntity>, world: World) : Zombie
             .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.2645)
             .add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 0.3)
             .add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 3.0)
+    }
+
+    override fun initialize(
+        world: ServerWorldAccess,
+        difficulty: LocalDifficulty,
+        spawnReason: SpawnReason?,
+        entityData: EntityData?,
+        entityNbt: NbtCompound?
+    ): EntityData? {
+        val result = super.initialize(world, difficulty, spawnReason, entityData, entityNbt)
+        setCanPickUpLoot(false)
+        return result
+    }
+
+    override fun initEquipment(random: Random?, localDifficulty: LocalDifficulty?) {
+        super.initEquipment(random, localDifficulty)
+        equipStack(EquipmentSlot.HEAD, ItemStack.EMPTY)
+        equipStack(EquipmentSlot.CHEST, ItemStack.EMPTY)
+        equipStack(EquipmentSlot.LEGS, ItemStack.EMPTY)
+        equipStack(EquipmentSlot.FEET, ItemStack.EMPTY)
     }
 
     override val attackAnimationName: String
